4d5b26788a8870edb35da0013baacc1c416dc652
Busetdah <apriantowpj@gmail.com>
2021-10-04 04:05:07 +0000

    change to ancient

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 The ancientOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_miatoll.mk
+    $(LOCAL_DIR)/ancient_miatoll.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_miatoll-user \
-    lineage_miatoll-userdebug \
-    lineage_miatoll-eng
+    ancient_miatoll-user \
+    ancient_miatoll-userdebug \
+    ancient_miatoll-eng
